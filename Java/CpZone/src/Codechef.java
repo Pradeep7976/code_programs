fb65fac3db797f644757d0b27ce1c8a2de044604
Pradeep <phantompradeep200@gmail.com>
2024-12-28 17:16:08 +0530

    update

--- Java/CpZone/src/Codechef.java
+++ Java/CpZone/src/Codechef.java
@@ -5,23 +5,5 @@
 //        int t;
 //        Scanner sc = new Scanner(System.in);
 //        t = sc.nextInt();
-//        while (t-- > 0) {
-//            int a, b;
-//            a = sc.nextInt();
-//            b = sc.nextInt();
-//            int ans = 0;
-//            while (a != b) {
-//                if (a > b) {
-//                    int c = Math.ceilDiv(a, 2);
-//                    a = a - c;
-//                    ans += c;
-//                } else {
-//                    int c = Math.ceilDiv(b, 2);
-//                    b = b - c;
-//                    ans += c;
-//                }
-//            }
-//            System.out.println(ans);
-//        }
 //    }
 //}
